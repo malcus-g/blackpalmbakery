854d567a8c13a9cefb2acdb221bad96665f0f299
Garrett <garrettcouch97@gmail.com>
2024-11-28 22:50:52 -0500

    implement home page

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,104 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+  <div id="app">
+    <header>
+      <img class="logo" src="/header_logo.jpeg" alt="logo" @click="this.$router.push({name: 'home'})"/>
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <router-link :to="{name: 'home'}"> Home </router-link>
+        <router-link :to="{name: ''}">About</router-link>
+        <router-link :to="{name: ''}">Gallery</router-link>
+        <router-link :to="{name: ''}">Contact</router-link>
       </nav>
-    </div>
-  </header>
+    </header>
 
-  <RouterView />
+    <main>
+      <router-view />
+    </main>
+    <footer>
+      <p>&copy; 2024 The Black Palm Bakery. All rights reserved.</p>
+    </footer>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+
+#app {
+  display: grid;
+  grid-template-areas:
+  "header"
+  "main"
+  "footer";
+  grid-template-rows: auto 1fr auto;
+  min-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+header {
+  grid-area: header;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: auto;
+  z-index: 9999;
+  background-color: #ffffff;
+  padding: 0 1em;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+}
+
+main {
+  grid-area: main;
+  margin-top: 6rem;
+}
+
+footer {
+  grid-area: footer;
+  padding: .2rem;
+  color: black;
+  text-align: center;
+  border-radius: 5px;
+}
+
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: .5rem 1rem;
+  background-color: white;
+}
+
+header img {
+  max-width: 100px;
+  border-radius: 5px;
+  cursor: pointer;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+  display: flex;
+  justify-content: flex-end;
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  text-decoration: none;
+  color: #000000;
+  margin: 0px 1em;
+  padding: .4em 1em;
+  border-radius: 8px;
+  text-align: center;
 }
 
-nav a:first-of-type {
-  border: 0;
+.router-link-active{
+    font-weight: bold;
+    color: black;
 }
+
+
 
 @media (min-width: 1024px) {
   header {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,62 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div class="home">
+    <img id="logo" src="/public/header_logo.jpeg" alt="">
+    <div id="heading-line">
+      <h1 id="home-title">
+        e komo mai!
+      </h1>
+      <h2 id="intro-message">
+        WELCOME
+      </h2>
+    </div>
+  </div>
+  <div class="background-image">
+    <img src="/public/2.png" alt="">
+  </div>
+</template>
+
+<script>
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+
+.home{
+  background-color: white;
+  width: 100vw;
+  border-radius: 10px;
+  margin-top: 2rem;
+}
+
+#logo{
+  display: block;
+  margin: 0 auto;
+}
+
+#home-title{
+    display: flex;
+    font-family: Shirkhand;
+    color: black;
+    justify-content: center;
+    font-size: 8rem;
+    text-shadow: 1px 1px 2px rgb(77, 77, 77);
+    padding: .5rem 0rem;
+}
+
+#intro-message{
+  color: black;
+  text-align: center;
+  font-size: 3rem;
+}
+
+.background-image{
+  width: 100vw;
+}
+
+.background-image > img{
+  object-fit: cover;
+  width: 100%;
+  height: 55em;
+}
+
+</style>
